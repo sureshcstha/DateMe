0e2ab1d888b3cace860ffe187f3233bec993ad16
Suresh Shrestha <sureshcstha@gmail.com>
2024-02-13 21:21:21 -0700

    Linking to Second View

--- DateMe/Controllers/HomeController.cs
+++ DateMe/Controllers/HomeController.cs
@@ -10,5 +10,10 @@ namespace DateMe.Controllers
         {
             return View();
         }
+
+        public IActionResult FillOutApplication()
+        {
+            return View("DatingApplication");
+        }
     }
 }
