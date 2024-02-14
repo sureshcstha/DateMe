7827ffa90a701c95c0e1cbf8763541d5114ed4ce
Suresh Shrestha <sureshcstha@gmail.com>
2024-02-13 21:36:55 -0700

    Models

---
+++ DateMe/Models/Application.cs
@@ -0,0 +1,13 @@
+﻿namespace DateMe.Models
+{
+    public class Application
+    {
+        public int ApplicationID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Major {  get; set; }
+        public string Occupation { get; set; }
+    }
+}
