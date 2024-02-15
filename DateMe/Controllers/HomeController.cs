bae0bb09345ae5357e3eb2072f41fec520f98c3a
Suresh Shrestha <sureshcstha@gmail.com>
2024-02-14 17:32:07 -0700

    Connecting to the Database

--- DateMe/Controllers/HomeController.cs
+++ DateMe/Controllers/HomeController.cs
@@ -6,6 +6,12 @@ namespace DateMe.Controllers
 {
     public class HomeController : Controller
     {
+        private DatingApplicationContex _context;
+
+        public HomeController(DatingApplicationContex temp) // constructor
+        {
+            _context = temp;
+        }
         public IActionResult Index()
         {
             return View();
@@ -18,6 +24,9 @@ namespace DateMe.Controllers
         [HttpPost]
         public IActionResult DatingApplication(Application response)
         {
+            _context.Applications.Add(response); // add record to the database
+            _context.SaveChanges();
+
             return View("Confirmation", response);
         }
     }
